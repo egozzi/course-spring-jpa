7a21e11659556122f210a49f58a935f87f938004
Erico Murilo Gozzi <erico@llee.tech>
2021-05-18 10:49:40 -0300

    Added service and repository layers.

--- src/main/java/com/pluralsight/conference/model/Registration.java
+++ src/main/java/com/pluralsight/conference/model/Registration.java
@@ -1,10 +1,32 @@
 package com.pluralsight.conference.model;
 
-import javax.validation.constraints.NotEmpty;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "REGISTRATIONS")
+@NamedQueries({
+        @NamedQuery(name = Registration.REGISTRATION_REPORT, query = Registration.REGISTRATION_REPORT_JPQL)
+})
 public class Registration {
 
+    public static final String REGISTRATION_REPORT = "registrationReport";
+
+    public static final String REGISTRATION_REPORT_JPQL = "select new com.pluralsight.conference.model.RegistrationReport" +
+            "(r.name, c.name, c.description) " +
+            "from Registration r, Course c " +
+            "where r.id = c.registration.id";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
     @NotEmpty
+    @Column(name = "NAME")
     private String name;
 
     public String getName() {
@@ -13,5 +35,25 @@ public class Registration {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "registration", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Course> courses = new ArrayList<>();
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 }
--- src/main/java/com/pluralsight/conference/model/User.java
+++ src/main/java/com/pluralsight/conference/model/User.java
@@ -1,10 +1,30 @@
 package com.pluralsight.conference.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "CONF_USERS")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "USER_ID")
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
     private String firstname;
+    @Column(name = "LAST_NAME")
     private String lastname;
+    @Column(name = "AGE")
     private int age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFirstname() {
         return firstname;
